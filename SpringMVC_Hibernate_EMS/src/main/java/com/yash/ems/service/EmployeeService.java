186f9c6cd16462e3d66289fc66c0c93d508541ad
pawar-shailesh <89083005+pawar-shailesh@users.noreply.github.com>
2021-09-28 10:31:07 +0530

    Add files via upload

---
+++ SpringMVC_Hibernate_EMS/src/main/java/com/yash/ems/service/EmployeeService.java
@@ -0,0 +1,28 @@
+package com.yash.ems.service;
+
+
+import com.yash.ems.exception.EmployeeIdNotFoundException;
+import com.yash.ems.exception.InvalidNameException;
+import com.yash.ems.model.Employee;
+
+/**
+ * EmployeeService Interface for declaring additional methods
+ * 
+ * @author shailesh.pawar
+ *
+ */
+public interface EmployeeService {
+	/*
+	 * this getid method is used for getting records by id from table employee
+	 */
+	public Employee getEmployeeById(int empid) throws EmployeeIdNotFoundException;
+
+	/*
+	 * this getname method is used for getting records by Name from table employee
+	 */
+	public Employee getEmployeeByName(String empname) throws InvalidNameException;
+
+	
+	
+	
+}
